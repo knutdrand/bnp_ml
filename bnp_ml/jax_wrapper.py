b896af6394353ec96456b378ee342e2f21094f11
Knut Rand <knutdrand@gmail.com>
2023-03-20 15:26:58 +0100

    Some updates

--- bnp_ml/jax_wrapper.py
+++ bnp_ml/jax_wrapper.py
@@ -29,13 +29,19 @@ def get_log_likelihood_function(distribution_class: type, data: np.ndarray):
 def estimate_fisher_information(model: Distribution, n: int = 10000000):
     n //= math.prod(model.event_shape)
     x = model.sample((n,))
-    # print(x)
     f = get_log_likelihood_function(model.__class__, x)
     hessian = jax.hessian(f, argnums=list(range(len(model.parameters))))(*model.parameters)
-    # print(hessian)
-    for H in hessian:
-        print([h.shape for h in H])
-    return H
-    return tuple(tuple(-np.mean(h, axis=-1) for h in row)
-                 for row in hessian)
+    return hessian
+# return tuple(tuple(-np.mean(h, axis=-1) for h in row)
+#                  for row in hessian)
 
+
+def estimate_gd(distribution, data, learning_rate=0.01, n_iterations=100):
+    if not isinstance(data, tuple):
+        data = (data, )
+    for i in range(n_iterations):
+        grad_func = jax.grad(lambda *params: -np.mean(distribution.__class__(*params).log_prob(*data)),
+                             argnums=list(range(len(distribution.parameters))))
+        grads = grad_func(*distribution.parameters)
+        distribution = distribution.__class__(*(param-grad*learning_rate for param, grad in zip(distribution.parameters, grads)))
+    return distribution
--- tests/test_jax_wrapper.py
+++ tests/test_jax_wrapper.py
@@ -1,10 +1,11 @@
-from logarray.logarray import log_array, LogArray
+from logarray import log_array, LogArray, set_backend, as_log_array
 import numpy as np
 import pytest
 
-from bnp_ml.jax_wrapper import estimate_fisher_information, Distribution
+from bnp_ml.jax_wrapper import estimate_fisher_information, Distribution, estimate_gd
 import jax.numpy as jnp
 from jax import random
+set_backend(jnp)
 
 
 class NormalDistribution:
@@ -26,7 +27,6 @@ class NormalDistribution:
         return jnp.log(1/(self.scale*jnp.sqrt(2*jnp.pi))) - 0.5*(data-self.loc)**2/self.scale**2
 
         r = np.log(self.probability(data))
-        print(r)
         return r
 
     def probability(self, data):
@@ -48,7 +48,7 @@ class Bernoulli:
 
     @property
     def parameters(self):
-        return (self.p, )
+        return [self.p]
 
     def log_prob(self, X):
         return jnp.log(self.p)*X + jnp.log(1-self.p) * (1-X)
@@ -68,15 +68,26 @@ def standard_normal() -> Distribution:
 def bernoulli() -> Distribution:
     return Bernoulli(0.4)
 
+
+@pytest.fixture
+def bernoulli_raw() -> Distribution:
+    return Bernoulli(np.array(0.9))
+
+
 # return dists.Normal(torch.tensor([1.0]), torch.tensor([1.0]))
 
 
 def test_fisher_information(standard_normal):
     I = estimate_fisher_information(standard_normal, n=10)
-    print(I)
+
 
 def test_fisher_information(bernoulli):
     I = estimate_fisher_information(bernoulli, n=100000)
-    print(I)
     true = 1/bernoulli.p/(1-bernoulli.p)
-    assert np.abs(I[0]-true) < 0.1
+    assert np.abs(I[0][0]-true) < 0.1
+
+
+def test_estimate(bernoulli, bernoulli_raw):
+    X = bernoulli.sample((1000, ))
+    dist = estimate_gd(bernoulli_raw, X, n_iterations=1000)
+    assert np.abs(dist.p-bernoulli.p) < 0.1
