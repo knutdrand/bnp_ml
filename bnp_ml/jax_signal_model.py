3cd562052cb78e708b8bb66d1f80e5f0163960e0
Knut Rand <knutdrand@gmail.com>
2023-03-23 15:57:40 +0100

    Passing test for model

--- bnp_ml/jax_signal_model.py
+++ bnp_ml/jax_signal_model.py
@@ -19,6 +19,12 @@ class _JaxSignalModel:
         self.fragment_length_distribution = fragment_length_distribution
         self._max_fragment_length = len(self.fragment_length_distribution)-1
         self._area_size = len(self.binding_affinity)
+        self._tot_probs = xp.array([self.fragment_length_distribution[1:position+2].sum() + self.fragment_length_distribution[1:(self._area_size-position+1)].sum()
+                                    for position in range(self._area_size)])
+
+    def tot_prob(self, position):
+        return self.fragment_length_distribution[1:position+2].sum() + self.fragment_length_distribution[1:(self._area_size-position+1)]
+
 
     def __repr__(self):
         return f'{self.__class__.__name__}({self.binding_affinity}, {self.fragment_length_distribution})'
@@ -57,19 +63,19 @@ class _JaxSignalModel:
     def log_prob(self, X: Union[Tuple[int, str], List[Tuple[int, str]]]):
         return xp.log(self.probability(X))
 
-    def probability(self, X: Union[Tuple[int, str], List[Tuple[int, str]]]):
-        if isinstance(X, list):
-            return xp.array([self.probability(x) for x in X])
-        position, strand = X
-        if strand == '+':
-            index = slice(position, position+self._max_fragment_length)
-        else:
-            index = slice(
-                position,
-                position-self._max_fragment_length if position-self._max_fragment_length >= 0 else None,
-                -1)
-        return xp.sum(
-            self.fragment_length_distribution[1:] * 0.5*self._padded_affinity[index])
+    # def probability(self, X: Union[Tuple[int, str], List[Tuple[int, str]]]):
+    #     if isinstance(X, list):
+    #         return xp.array([self.probability(x) for x in X])
+    #     position, strand = X
+    #     if strand == '+':
+    #         index = slice(position, position+self._max_fragment_length)
+    #     else:
+    #         index = slice(
+    #             position,
+    #             position-self._max_fragment_length if position-self._max_fragment_length >= 0 else None,
+    #             -1)
+    #     return xp.sum(
+    #         self.fragment_length_distribution[1:] * 0.5*self._padded_affinity[index])
 
     def domain(self):
         pos = {(self._max_fragment_length + binding_pos-fragment_length, '+')
@@ -88,6 +94,8 @@ class JaxSignalModel(_JaxSignalModel):
     Thus is should be lower on the edges
     '''
 
+        
+
     def probability(self, X: Union[Tuple[int, str], List[Tuple[int, str]]]):
         if isinstance(X, list):
             return xp.array([self.probability(x) for x in X])
@@ -100,9 +108,10 @@ class JaxSignalModel(_JaxSignalModel):
                 position-self._max_fragment_length if position-self._max_fragment_length >= 0 else None,
                 -1)
         affinity = self.binding_affinity[index]
-        length_prob = self.fragment_length_distribution[1:1+len(affinity)]
-        length_prob = length_prob/length_prob.sum()
-        return xp.sum(0.5*affinity*length_prob/length_prob.sum())
+        tot_probs = self._tot_probs[index]
+        length_prob = self.fragment_length_distribution[1:1+len(affinity)]/tot_probs
+        # length_prob = length_prob/length_prob.sum()
+        return xp.sum(affinity*length_prob)
 
     def domain(self):
         return {(pos, strand) for pos in range(len(self.binding_affinity)) for strand in ('+', '-')}
@@ -117,11 +126,11 @@ class JaxSignalModel(_JaxSignalModel):
         print(pos, s_pos, s_neg)
         tot = s_pos + s_neg
         print('#', pos, (s_pos/tot, s_neg/tot))
-        reverse = rng.choice([False, True], p=(s_pos/tot, s_neg/tot))
+        reverse = rng.choice([False, True], p=np.array((s_pos/tot, s_neg/tot)))
         if not reverse:
-            pos = pos-rng.choice(np.arange(pre.size), p=pre/s_pos)
+            pos = pos-rng.choice(np.arange(pre.size), p=np.array(pre/s_pos))
         else:
-            pos = pos+rng.choice(np.arange(post.size), p=post/s_neg)
+            pos = pos+rng.choice(np.arange(post.size), p=np.array(post/s_neg))
         strand = '-' if reverse else '+'
         assert pos >= 0 and pos < self._area_size, (reverse, pos, fragment_length)
         return pos, strand
